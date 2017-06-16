4ec77814763bd30fa42543c7d6c334bd72ccdd43
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2017-06-16 13:42:02 -0400

    chokepoint utility portal type vector

--- inc/datasetChokePoint.h
+++ inc/datasetChokePoint.h
@@ -9,6 +9,7 @@
 #define COMMON_CPP_DATASET_CHOKEPOINT_H
 
 #include <string>
+#include <vector>
 using namespace std;
 
 class ChokePoint 
@@ -17,20 +18,21 @@ public:
     // --- possible sequence information ---
 
     enum PortalType { ENTER, LEAVE };                       // Portal types indicating the direction the in which portal is crossed
+    static const vector<PortalType> PORTAL_TYPES;           // Vector of all portal types for convenience iterator
     static const int PORTAL_TYPE_QUANTITY;                  // Number of portals types (1-based indexes)
     static const int PORTAL_QUANTITY;                       // Number of portals employed (1-based indexes)
     static const int SESSION_QUANTITY;                      // Number of sequenced individual trajectory video sessions (1-based indexes)
     static const int SESSION_SIMULTANEOUS_INDIVIDUALS;      // Additional video session with simultaneous individual trajectories
     static const int CAMERA_QUANTITY;                       // Number of camera viewpoints per session/portal (1-based indexes)
     static const int INDIVIDUAL_QUANTITY;                   // Number of individuals (1-based indexes, warning: #8 often omitted)
-    static const int TOTAL_SEQUENCES;                       // Total sequence combinations
+    static const int TOTAL_SEQUENCES;                       // Total sequence combinations    
 
     // --- combine sequence information ---
 
     // returns 'P#T_S#_C#', or optionally 'P#T_S#_C#/ID####' if a valid ID is specified
     // returns '' if either of the video sequence indexes are invalid values
     static string getSequenceString(int portal, PortalType type, int session, int camera, int id = 0);
-    static string getIndividualID(int id, bool withPrefixID = false);
+    static string getIndividualID(int id, bool withPrefixID = false);    
 
     // --- directory names ---
 
